c213b14a274f0ebeb09a03c33623cab08b6cb7b0
appdever01 <appdever01@gmail.com>
2024-03-07 11:13:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,36 +9,30 @@ app.use(express.json());
 
 const token = process.env.TOKEN;
 console.log("Started");
+// Updated code to send button message if user sends "Hi" and handle classname input
 app.post("/webhook", async (req, res) => {
   try {
     const data = req.body;
-
     console.log(data);
-
     await insertUser(data);
 
     let response_msg = "";
 
-    // send_template(
-    //   "firstt_message",
-    //   "https://telegra.ph/file/2894988ef8b29096c7aeb.jpg",
-    //   data
-    // );
-
-    const button = [
-      { id: "btn_1", title: "Submit" },
-      { id: "btn_2", title: "Cancel" },
-    ];
-    send_button("Button testinng ", button, data);
-
-    if (data.type == "interactive" && data.btn_id == "btn_1") {
-      if (send_message("Provide a name for the class", data)) {
-        if (data.type == "text") {
-          send_message(`You said ${data.msg}`, data);
-        }
+    if (data.type === "text" && data.msg.toLowerCase() === "hi") {
+      const button = [
+        { id: "btn_1", title: "Submit" },
+        { id: "btn_2", title: "Cancel" },
+      ];
+      send_button("Please select an option:", button, data);
+    } else if (data.type === "interactive" && data.btn_id === "btn_1") {
+      if (
+        send_message("Provide a name for the class", data) &&
+        data.type === "text"
+      ) {
+        send_message(`You said ${data.msg}`, data);
       }
     } else {
-      response_msg = "Fuck yoouu";
+      response_msg = "Error: Invalid request";
     }
 
     res.json({
